c1df020b6c1af1c49c61f63dbf307806fd539617
Baha Barut <20201129503@ogr.akdeniz.edu.tr>
2024-02-10 13:40:46 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,17 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+Person person = new Person();
+person.NationalIdentity= 00000;
+person.FirstName = "Baha";
+person.LastName = "Barut";
+person.DateOfBirthYear = 2000;
+List<string> sehirler = new List<string> { "Ankara", "Istanbul", "Izmir" };
+sehirler.Add("Adana");
+foreach (var item in sehirler)
+{
+    Console.WriteLine(item);
+}
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person);
